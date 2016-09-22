a3757791d2fcfd6b48ebd600d150ff8a86d304a4
kbchun <kbchun5712@gmail.com>
2016-09-21 19:50:24 -0700

    "Finished a SUPER OPTIMAL version of findNRooksSolution and countNRooksSolution"

--- src/solvers.js
+++ src/solvers.js
@@ -16,18 +16,63 @@
 
 
 window.findNRooksSolution = function(n) {
-  var solution = undefined; //fixme
+  debugger;
+  var board = new Board({'n': n});
+  var solutions = filterPossibilities(getFinalBoards(n, 0, board));
 
-  console.log('Single solution for ' + n + ' rooks:', JSON.stringify(solution));
-  return solution;
+  // console.log('Single solution for ' + n + ' rooks:', JSON.stringify(solutions));
+  return solutions[0].rows();
 };
+
+var getFinalBoards = function(n, row, board) {
+  var boards = [];
+  // var solutions = [];
+
+  if (board._isInBounds(row, 0)) {
+    for (var i = 0; i < n; i++) {
+      // var copy = new Board(board.rows());
+      var copyMatrix = [];
+      for (var j = 0; j < board.rows().length; j++) {
+        copyMatrix.push(board.rows()[j].slice());
+      }
+      var copy = new Board(copyMatrix);
+      copy.togglePiece(row, i);
+      // boards.push(copy);
+      boards.push(getFinalBoards(n, row + 1, copy));
+    }
+  } else {
+    // console.log(board);
+    return board;
+  }
+
+  return boards;
+};
+
+
+var filterPossibilities = function(boards) {
+  var solutions = [];
+  boards = _.flatten(boards);
+  for (var j = 0; j < boards.length; j++) {
+    // for (var k = 0; k < boards[j].length; k++) {
+    if (!boards[j].hasAnyRooksConflicts()) {
+      solutions.push(boards[j]);
+    }
+  }
+  return solutions;
+};
+
+// var x = new Board({n:2});
+// debugger; 
+// var y = getFinalBoards(2, 0, x);
 
 // return the number of nxn chessboards that exist, with n rooks placed such that none of them can attack each other
 window.countNRooksSolutions = function(n) {
-  var solutionCount = undefined; //fixme
+  // var solutionCount = undefined; //fixme
+  var board = new Board({'n': n});
+  var solutions = filterPossibilities(getFinalBoards(n, 0, board));
 
   console.log('Number of solutions for ' + n + ' rooks:', solutionCount);
-  return solutionCount;
+  return solutions.length;
 };
 
 // return a matrix (an array of arrays) representing a single nxn chessboard, with n queens placed such that none of them can attack each other
